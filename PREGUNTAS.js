b40c59d6c1e7a304f47c3f20b0658e3deadd4f09
Jose <arck1997@hotmail.com>
2023-08-11 12:46:49 -0600

    archivos

---
+++ PREGUNTAS.js
@@ -0,0 +1,117 @@
+alert("Por favor responder las respuestas que se te piden con lo que se te indica");
+let nombre =prompt("Como te llamas")
+let frio= prompt("Te gusta el frio");
+let color= prompt("Que color te gusta mas negro o cafe");
+let parteAlta= prompt("Te gusta las chamarra o sudadera");
+let parteBja=prompt("Te gustan los pants o patalon");
+let calzado=prompt("Que prefieres tenis o botas ")
+
+if((color.toLocaleLowerCase() == "cafe") && (parteAlta.toLocaleLowerCase() == "chamarra")
+&& (parteBja.toLocaleLowerCase() == "pants") && (calzado.toLocaleLowerCase() == "tenis")){
+alert(`Hola ${nombre} te sugerimos este atuendo para ti ☺️`);
+document.write("<img src='imagenesvestimenta/pants.jpg' width='200' height='200'>")
+document.write("<img src='imagenesvestimenta/chamarra cafe.webp' width='200' height='200'>")
+document.write("<img src='imagenesvestimenta/tenis,cafe,converse.jpeg' width='200' height='200'>")
+
+
+}else if((color.toLocaleLowerCase() == "cafe") && (parteAlta.toLocaleLowerCase() == "chamarra")
+&& (parteBja.toLocaleLowerCase() == "pantalon") && (calzado.toLocaleLowerCase() == "tenis")){
+    alert(`Hola ${nombre} te sugerimos este atuendo para ti ☺️`);
+    document.write("<img src='imagenesvestimenta/pantalon,chamarra,tenis,cafe.jpg' width='200' height='200'>")
+}else if((color.toLocaleLowerCase() == "cafe") && (parteAlta.toLocaleLowerCase() == "sudadera")
+&& (parteBja.toLocaleLowerCase() == "pants") && (calzado.toLocaleLowerCase() == "tenis")){
+    alert(`Hola ${nombre} te sugerimos este atuendo para ti ☺️`);
+    document.write("<img src='imagenesvestimenta/pants.jpg' width='200' height='200'>")
+    document.write("<img src='imagenesvestimenta/sudadera,cafe.jpg' width='200' height='200'>")
+    document.write("<img src='imagenesvestimenta/tenis,cafe,converse.jpeg' width='200' height='200'>")
+
+}else if((color.toLocaleLowerCase() == "cafe") && (parteAlta.toLocaleLowerCase() == "sudadera")
+&& (parteBja.toLocaleLowerCase() == "pantalon") && (calzado.toLocaleLowerCase() == "tenis")){
+    alert(`Hola ${nombre} te sugerimos este atuendo para ti ☺️`);
+    document.write("<img src='imagenesvestimenta/pantalon.webp' width='200' height='200'>")
+    document.write("<img src='imagenesvestimenta/sudadera,cafe.jpg' width='200' height='200'>")
+    document.write("<img src='imagenesvestimenta/tenis,cafe,converse.jpeg' width='200' height='200'>")
+
+}
+else if((color.toLocaleLowerCase() == "cafe") && (parteAlta.toLocaleLowerCase() == "chamarra")
+&& (parteBja.toLocaleLowerCase() == "pants") && (calzado.toLocaleLowerCase() == "botas")){
+    alert(`Hola ${nombre} no tenemos un atuendo sobre los datos que introduciste,  te sugiremos uno ☺️`);
+    document.write("<img src='imagenesvestimenta/pants.jpg' width='200' height='200'>")
+document.write("<img src='imagenesvestimenta/chamarra cafe.webp' width='200' height='200'>")
+document.write("<img src='imagenesvestimenta/tenis,cafe,converse.jpeg' width='200' height='200'>")
+
+
+}
+else if((color.toLocaleLowerCase() == "cafe") && (parteAlta.toLocaleLowerCase() == "chamarra")
+&& (parteBja.toLocaleLowerCase() == "pantalon") && (calzado.toLocaleLowerCase() == "botas")){
+    alert(`Hola ${nombre} te sugerimos este atuendo para ti ☺️`);
+    document.write("<img src='imagenesvestimenta/pantalon,botas,cafe.webp' width='200' height='200'>")
+
+}else if((color.toLocaleLowerCase() == "cafe") && (parteAlta.toLocaleLowerCase() == "sudadera")
+&& (parteBja.toLocaleLowerCase() == "pants") && (calzado.toLocaleLowerCase() == "botas")){
+    alert(`Hola ${nombre} no tenemos un atuendo sobre los datos que introduciste,  te sugiremos uno ☺️`);
+document.write("<img src='imagenesvestimenta/pants.jpg' width='200' height='200'>")
+document.write("<img src='imagenesvestimenta/sudadera,cafe.webp' width='200' height='200'>")
+document.write("<img src='imagenesvestimenta/tenis,cafe,converse.jpeg' width='200' height='200'>")
+
+}else if((color.toLocaleLowerCase() == "cafe") && (parteAlta.toLocaleLowerCase() == "sudadera")
+&& (parteBja.toLocaleLowerCase() == "pantalon") && (calzado.toLocaleLowerCase() == "botas")){
+    alert(`Hola ${nombre} te sugerimos este atuendo para ti ☺️`);
+    document.write("<img src='imagenesvestimenta/pantalon.webp' width='200' height='200'>")
+    document.write("<img src='imagenesvestimenta/sudadera,cafe.jpg' width='200' height='200'>")
+    document.write("<img src='imagenesvestimenta/botas,cafe.jpeg' width='200' height='200'>")
+}
+
+
+
+else if((color.toLocaleLowerCase() == "negro") && (parteAlta.toLocaleLowerCase() == "chamarra")
+&& (parteBja.toLocaleLowerCase() == "pants") && (calzado.toLocaleLowerCase() == "tenis")){
+    alert(`Hola ${nombre} te sugerimos este atuendo para ti ☺️`);
+document.write("<img src='imagenesvestimenta/chamarra,pant,tenis,negro.jpg' width='200' height='200'>")
+
+}else if((color.toLocaleLowerCase() == "negro") && (parteAlta.toLocaleLowerCase() == "chamarra")
+&& (parteBja.toLocaleLowerCase() == "pantalon") && (calzado.toLocaleLowerCase() == "tenis")){
+    alert(`Hola ${nombre} te sugerimos este atuendo para ti ☺️`);
+     document.write("<img src='imagenesvestimenta/Negro pantalon tenis,chamarra.jpeg' width='200' height='200'>")
+
+}else if((color.toLocaleLowerCase() == "negro") && (parteAlta.toLocaleLowerCase() == "sudadera")
+&& (parteBja.toLocaleLowerCase() == "pants") && (calzado.toLocaleLowerCase() == "tenis")){
+    alert(`Hola ${nombre} te sugerimos este atuendo para ti ☺️`);
+    document.write("<img src='imagenesvestimenta/pants,sudadera,tenis.negro.jpeg' width='200' height='200'>")
+
+}else if((color.toLocaleLowerCase() == "negro") && (parteAlta.toLocaleLowerCase() == "sudadera")
+&& (parteBja.toLocaleLowerCase() == "pantalon") && (calzado.toLocaleLowerCase() == "tenis")){
+    alert(`Hola ${nombre} te sugerimos este atuendo para ti ☺️`);
+    document.write("<img src='imagenesvestimenta/sudadera,pantalo,tenis,negro.webp' width='200' height='200'>")
+
+}
+else if((color.toLocaleLowerCase() == "negro") && (parteAlta.toLocaleLowerCase() == "chamarra")
+&& (parteBja.toLocaleLowerCase() == "pants") && (calzado.toLocaleLowerCase() == "botas")){
+    alert(`Hola ${nombre} no tenemos un atuendo sobre los datos que introduciste,  te sugiremos uno ☺️`);
+    document.write("<img src='imagenesvestimenta/negro,chamarra,botas,pantalon.jpg' width='200' height='200'>")
+}
+else if((color.toLocaleLowerCase() == "negro") && (parteAlta.toLocaleLowerCase() == "chamarra")
+&& (parteBja.toLocaleLowerCase() == "pantalon") && (calzado.toLocaleLowerCase() == "botas")){
+    alert(`Hola ${nombre} te sugerimos este atuendo para ti ☺️`);
+    document.write("<img src='imagenesvestimenta/negro,chamarra,botas,pantalon.jpg' width='200' height='200'>")
+
+}else if((color.toLocaleLowerCase() == "negro") && (parteAlta.toLocaleLowerCase() == "sudadera")
+&& (parteBja.toLocaleLowerCase() == "pants") && (calzado.toLocaleLowerCase() == "botas")){
+    alert(`Hola ${nombre} no tenemos un atuendo sobre los datos que introduciste,  te sugiremos uno ☺️`);
+    document.write("<img src='imagenesvestimenta/sudadera,pantalon,bota,negro.jpg' width='200' height='200'>")
+
+}else if((color.toLocaleLowerCase() == "negro") && (parteAlta.toLocaleLowerCase() == "sudadera")
+&& (parteBja.toLocaleLowerCase() == "pantalon") && (calzado.toLocaleLowerCase() == "botas")){
+ alert(`Hola ${nombre} te sugerimos este atuendo para ti ☺️`);
+    document.write("<img src='imagenesvestimenta/sudadera,pantalon,bota,negro.jpg' width='200' height='200'>")
+}
+else if(frio.toLocaleLowerCase() == "no"){
+    alert("No existe combinacion")
+}
+else{
+    alert("No exite combinacion lo sentimos")
+}
+
+
+
+//contador de numeros  
---
+++ juego.js
@@ -0,0 +1,60 @@
+function game(parametro1,parametro2){
+    let final;
+    if(parametro1 == "Papel" & parametro2 == "Papel"){
+                   final= "Empate";
+    }
+    if(parametro1 ==  "Piedra" & parametro2 == "Piedra"){
+        final= "Empate";
+}
+if(parametro1 ==  "Tijeras" & parametro2 == "Tijeras"){
+final= "Empate";
+}
+    else if(parametro1 =="Papel" & parametro2== "Piedra"){
+                       final="J1"
+    }
+    else if(parametro1 =="Papel" & parametro2== "Tijeras"){
+        final="J2"
+}
+else if(parametro1 =="Tijeras" & parametro2== "Piedra"){
+final="J2"
+}else if(parametro1 =="Tijeras" & parametro2== "Papel"){
+final="J1"
+}
+else if(parametro1 =="Piedra" & parametro2== "Papel"){
+final="J2"
+}
+else if(parametro1 =="Piedra" & parametro2== "Tijeras"){
+final="J1"
+}
+
+
+
+else if(parametro2 =="Papel" & parametro1== "Piedra"){
+    final="J2"
+}
+else if(parametro2 =="Papel" & parametro1== "Tijeras"){
+final="J1"
+}
+
+
+
+else if(parametro2 =="Tijeras" & parametro1== "Piedra"){
+final="J1"
+}else if(parametro2 =="Tijeras" & parametro1== "Papel"){
+final="J2"
+}
+else if(parametro2 =="Piedra" & parametro1== "Papel"){
+final="J1"
+}
+else if(parametro2=="Piedra" & parametro1== "Tijeras"){
+final="J2"
+}
+
+
+return final;
+}
+
+const jugador1= prompt("Escriba una de las opciones del juego de piedra, papel o tijera");
+const jugador2= prompt("Escriba una de las opciones del juego de piedra, papel o tijera");
+
+console.log(game(jugador1,jugador2));
